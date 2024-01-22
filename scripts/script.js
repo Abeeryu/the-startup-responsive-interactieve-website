89e1cf03c6473ebc3f03e095434db41c72c94297
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2024-01-22 17:27:55 +0100

    changes 3

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,14 @@ const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 const popup = document.getElementById("popup");
 
+// Event listener for opening the popup
+
 openBtn.addEventListener("click", () =>{
     popup.classList.add("open");
     console.log("njnj")
 });
+
+// Event listener for closing the popup
 
 closeBtn.addEventListener("click", () =>{
     popup.classList.remove("open");
